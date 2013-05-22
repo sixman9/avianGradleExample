d11a5c1f50aecf6c09620e1a6c303a3cb1cbd8f6
sixman9 <sixman9@gmail.com>
2013-05-22 15:41:28 +0100

    HelloWorld application defintion and an attempt to resolve external avian components (no valid 'com.evocate' repository identified)

---
+++ src/main/java/com/fonotext/avian/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.fonotext.avian;
+
+/**
+ * @author richard.joseph
+ * Created: Wed May 22 14:56:38 BST 2013
+ */
+public class HelloWorld {
+   public static void main(String[] args) {
+		System.out.println("Hello World from this Avian executable!");
+   }
+}
